c268ff10684830c306d468983ff864099b89a93a
Anton Kostiuk <k.anton@wwg.it>
2024-12-07 03:02:37 +0200

    again

--- modules/mqtt/main.tf
+++ modules/mqtt/main.tf
@@ -114,7 +114,7 @@ resource "aws_ecs_task_definition" "mqtt" {
       interval    = 30
       timeout     = 10
       retries     = 5
-      startPeriod = 15
+      startPeriod = 20
     }
     logConfiguration = {
         logDriver = "awslogs"
